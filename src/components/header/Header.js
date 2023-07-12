99a2e9e0a8b9d767075a467c2757c21455636b38
Reneh7 <renehm2001@gmail.com>
2023-07-12 20:17:36 +0300

    class-17

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
 import "./Header.css"
+import { Link } from "react-router-dom";
 function Header()
 {
     return (
     <>
      <header className="header">
-       <h1>Tours</h1>
+       <h1>
+        <Link to="/">Home</Link>
+        Tours</h1>
      </header>
     </>)
 }
--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -1,14 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Tours.css";
 
-function Tours(props) {
+function Tours({ data }) {
   return (
-    <>
-        <div className="card">
-          <p>Name: {props.name}</p>
-          <img src={props.image} className="card-img-top" alt={props.name} />
-        </div>
-    </>
+    <div className="card-container">
+      {data.map((tour) => (
+        <Link key={tour.id} to={`/tours/${tour.id}`} className="card-link">
+          <div className="card">
+            <h3>Name: {tour.name}</h3>
+            <img src={tour.image} className="card-img-top" alt={tour.name} />
+          </div>
+        </Link>
+      ))}
+    </div>
   );
 }
 
 export default Tours;
+
+
